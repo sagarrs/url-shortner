00ae1e00f942280d4f89f8dc5549f32f6b4c44f2
sagar <sagarrs293@gmail.com>
2019-03-24 17:22:42 +0530

    user auth validate

---
+++ app/controllers/usersController.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+const {User} = require("../models/user")
+
+router.post("/register", function(req, res){
+    const body = req.body
+    const user = new User(body)
+
+    user.save()
+        .then((user) => {
+            res.send(user)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+})
+
+
+module.exports = {
+    userRouter: router
+}
